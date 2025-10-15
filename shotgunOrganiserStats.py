4cbcf5508072815a05d76579db5fffa30c2ab43e
MGousset <gousset.marviin@gmail.com>
2025-10-16 00:21:54 +0200

    first commit

--- shotgunOrganiserStats.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-filledOrgas = pd.read_excel('filledShotgunVenues.xlsx')
-filledOrgas = filledOrgas.drop(axis=1, columns=filledOrgas.columns[0])
-filledOrgas.to_sql("filledShotgunVenues.sql")
