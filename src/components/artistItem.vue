6c201279f916161c1c12cb47261ccd8de64a15cf
MGousset <gousset.marviin@gmail.com>
2025-11-05 20:04:06 +0100

    wip style artiste

--- src/components/artistItem.vue
+++ src/components/artistItem.vue
@@ -38,44 +38,42 @@ onMounted(() => {
         {{ name }}
       </h2>
       <div id="stylesContainer" class="w-100 flex flex-row flex-start">
-        <div v-for="style in props.styles" :key="style">
-          <span class="styleContainer">{{ style }}</span>
-        </div>
+        <span v-for="style in props.styles" :key="style" class="styleContainer">{{ style }}</span>
       </div>
     </div>
   </div>
   <DialogItem :id="modalId">
-    <div id="dialogContainer" class="w-100 h-100">
-      <div id="dialogBackground" class="w-100 h-100"></div>
-      <div id="dialogContent" class="flex flex-column w-100 h-100">
-        <div class="flex flex-column infosContainer">
+    <div id="dialogContainer" class="w-100 h-100 flex flex-column">
+      <div class="infosContainer">
+        <div id="artistImageBackground" class="w-100 h-100"></div>
+        <div id="infosContent" class="h-100 w-100 flex flex-column flex-between">
           <h2>
             {{ name }}
           </h2>
-          <div class="flex flex-between flex-align-end">
+          <div class="w-100 flex flex-between flex-align-end">
             <div id="stylesContainer" class="w-100 flex flex-row flex-start">
-              <div v-for="style in props.styles" :key="style">
-                <span class="styleContainer">{{ style }}</span>
-              </div>
+              <span v-for="style in props.styles" :key="style" class="styleContainer">{{
+                style
+              }}</span>
             </div>
             <div id="bookButton" class="flex flex-center flex-align-center"><h4>Book</h4></div>
           </div>
         </div>
-        <div id="detailsContainer" class="w-100 flex flex-column flex-around">
-          <div id="trackContainer" class="w-100">
-            <div id="trackContent" class="w-100 flex flex-column">
-              <SoundCloudSong
-                v-for="id in props.trackIds"
-                :key="id"
-                :id="id"
-                classes="trackItem"
-              ></SoundCloudSong>
-            </div>
+      </div>
+      <div id="detailsContainer" class="w-100 flex flex-column flex-around">
+        <div id="trackContainer" class="w-100">
+          <div id="trackContent" class="w-100 flex flex-column">
+            <SoundCloudSong
+              v-for="id in props.trackIds"
+              :key="id"
+              :id="id"
+              classes="trackItem"
+            ></SoundCloudSong>
           </div>
-          <div id="descriptionContainer" class="w-100">
-            <div id="descriptionContent" class="flex flex-column flex-center">
-              <p v-for="line in props.description" :key="line">{{ line }}</p>
-            </div>
+        </div>
+        <div id="descriptionContainer" class="w-100">
+          <div id="descriptionContent" class="flex flex-column flex-center">
+            <p v-for="line in props.description" :key="line">{{ line }}</p>
           </div>
         </div>
       </div>
@@ -140,7 +138,6 @@ onMounted(() => {
 }
 
 .infosContainer {
-  height: 128px;
   padding: 0.5rem;
   background-color: rgba(255, 255, 255, 0);
 
@@ -157,7 +154,7 @@ onMounted(() => {
 }
 
 #dialogContainer {
-  #dialogBackground {
+  #artistImageBackground {
     z-index: -1;
     position: absolute;
     background-image: v-bind(backgroundImage);
@@ -167,59 +164,74 @@ onMounted(() => {
     background-repeat: no-repeat;
   }
 
-  #dialogContent {
+  .infosContainer {
+    height: fit-content;
+    min-height: 13rem;
+    justify-items: center;
+    position: relative;
+    padding: 0rem;
+
+    #infosContent {
+      padding: 2rem;
+    }
+  }
+
+  #bookButton {
+    cursor: pointer;
+    visibility: visible;
+    width: 150px;
+    height: fit-content;
+    padding: 0.75rem;
+
+    background-color: rgba(255, 255, 255, 1);
+    &:hover {
+      background-color: rgba(255, 255, 255, 0.8);
+    }
+
+    h4 {
+      margin: 0px;
+      color: black !important;
+    }
+  }
+
+  #detailsContainer {
+    height: calc(100% - 128px - 2rem);
+    margin-top: 2rem;
     padding: 2rem;
 
-    #bookButton {
-      cursor: pointer;
-      visibility: visible;
-      width: 150px;
+    #trackContainer,
+    #descriptionContainer {
+      border-radius: 5px;
+      overflow-y: auto;
+      margin-bottom: 2rem;
+      max-height: 50%;
       height: fit-content;
-      padding: 0.75rem;
+      background-color: rgba(0, 0, 0, 0.5);
+    }
+    #trackContainer {
+      #trackContent {
+        padding: 1rem 2rem 1rem 2rem;
 
-      background-color: rgba(255, 255, 255, 1);
-      &:hover {
-        background-color: rgba(255, 255, 255, 0.8);
-      }
-
-      h4 {
-        margin: 0px;
-        color: black !important;
+        .trackItem {
+          margin-bottom: 1rem;
+          margin-top: 1rem;
+        }
       }
     }
 
-    #detailsContainer {
-      height: calc(100% - 128px - 2rem);
-      margin-top: 2rem;
+    #descriptionContainer {
+      padding: 2rem;
 
-      #trackContainer,
-      #descriptionContainer {
-        border-radius: 5px;
+      #descriptionContent {
         overflow-y: auto;
-        margin-bottom: 2rem;
-        max-height: 50%;
-        height: fit-content;
-        background-color: rgba(0, 0, 0, 0.5);
-      }
-      #trackContainer {
-        #trackContent {
-          padding: 1rem 2rem 1rem 2rem;
-
-          .trackItem {
-            margin-bottom: 1rem;
-            margin-top: 1rem;
-          }
-        }
-      }
-
-      #descriptionContainer {
-        padding: 2rem;
-
-        #descriptionContent {
-          overflow-y: auto;
-        }
       }
     }
   }
+}
+
+dialog:-internal-dialog-in-top-layer::backdrop {
+  background-image: v-bind(backgroundImage);
+  background-size: 33%;
+  filter: blur(20px) grayscale(0.2);
 }
 </style>
